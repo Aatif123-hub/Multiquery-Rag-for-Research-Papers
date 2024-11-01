7b42625f5ff7bd6636385f3b0967b06a7c01faf8
Aatif123-hub <71110011+Aatif123-hub@users.noreply.github.com>
2024-10-31 19:55:54 -0500

    Added huggingface embedding for the user to select

--- rag/embedding.py
+++ rag/embedding.py
@@ -1,12 +1,13 @@
 import os
 from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 load_dotenv()
 
 class Embeddings:
 
     def get_available_embeddings():
-        return ['small', 'large']
+        return ['small', 'large','huggingface']
 
     def get_embeddings(select_embedding):
         if select_embedding.lower() == 'small':
@@ -20,7 +21,12 @@ class Embeddings:
                 embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
             except Exception as e:
                 raise Exception(f"Cannot load large embedding model. Error: {e}")
+        elif select_embedding.lower() == 'huggingface':
+            try:
+                embeddings = HuggingFaceEmbeddings(model="sentence-transformers/all-MiniLM-L6-v2")
+            except Exception as e:
+                raise Exception(f"Cannot load huggingface embedding model. Error: {e}")
         else:
-            raise ValueError("Unsupported embedding. Choose 'small' or 'large'.")
+            raise ValueError("Unsupported embedding. Choose 'small','large' or 'huggingface'.")
         
         return embeddings
