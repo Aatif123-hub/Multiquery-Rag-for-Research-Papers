ccbdd518c48903e5e0056919b7298f9873f4e583
Aatif123-hub <71110011+Aatif123-hub@users.noreply.github.com>
2024-10-31 19:31:57 -0500

    Added Vectorstore

---
+++ rag/vectorstore.py
@@ -0,0 +1,46 @@
+import os
+from langchain_community.vectorstores import FAISS
+from langchain_community.vectorstores import Qdrant
+from langchain_community.vectorstores import Chroma
+
+
+
+class Vectorstore:
+   def get_available_vectorstores():
+      return ['faiss', 'qdrant', 'chroma']
+
+   def vectorization(store_select, text_chunks, embeddings):
+        if store_select.lower() == 'faiss':
+            try:
+                vectorstore = FAISS.from_texts(
+                    texts=text_chunks,
+                    embedding=embeddings
+                )
+            except Exception as e:
+                raise Exception(f"Cannot load FAISS vectorstore. Error: {e}")
+        
+        elif store_select.lower() == 'qdrant':
+            try:
+                vectorstore = Qdrant.from_texts(
+                    texts=text_chunks,
+                    url=os.getenv("QDRANT_URL"),
+                    api_key=os.getenv("QDRANT_API_KEY"),
+                    collection_name=os.getenv("COLLECTION_NAME"),
+                    embedding=embeddings
+                )
+            except Exception as e:
+                raise Exception(f"Cannot load Qdrant vectorstore. Error: {e}")
+            
+        elif store_select.lower() == 'chroma':
+            try:
+                vectorstore = Chroma.from_texts(
+                    texts=text_chunks,
+                    embedding=embeddings
+                )
+            except Exception as e:
+                raise Exception(f"Cannot load Chroma vectorstore. Error: {e}")
+        
+        else:
+            raise ValueError("Unsupported vector store type. Choose 'faiss', 'qdrant', or 'chroma'.")
+        
+        return vectorstore
